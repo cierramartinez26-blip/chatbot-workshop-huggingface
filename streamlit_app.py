76a5823f0c389c9295f5e70c6ef8b1d48a1a3b67
cierramartinez26-blip <cierra.martinez26@gmail.com>
2026-02-11 13:26:05 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,8 @@ import nltk
 def get_stopwords():
     nltk.download('stopwords')
 
-st.set_page_config(page_title="Chat with a friend on the works of Rabindranath Tagore", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
-st.title("Chat with a friend on the works of Rabindranath Tagore")
+st.set_page_config(page_title="Chat with a friend on the works of mychatbot", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.title("Chat with a friend on the works of mychatbot")
 
 
 if "messages" not in st.session_state.keys():  # Initialize the chat messages history
@@ -44,7 +44,7 @@ def load_data():
     token=st.secrets.hftoken,
     generate_kwargs={"temperature": 0.7, "top_k": 50, "top_p": 0.95},
     provider="auto",  # this will use the best provider available
-    system_prompt="""You are an expert on the work of Rabindranath Tagore.
+    system_prompt="""resond like a teacher professional but informative.
     Answer the question using the provided documents, which contain relevant excerpts from the work of Rabindranath Tagore.
     The context for all questions is the work of Rabindranath Tagore. Whenever possible, include a quotation from the provided excerpts of his work to illustrate your point.
     Respond using a florid but direct tone, typical of an early modernist writer.
